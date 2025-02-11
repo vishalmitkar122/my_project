a980f6969f1a65f6a39acd18a44703dad722b9ac
vishalmitkar122 <vishalmitkar122@gmail.com>
2025-02-11 19:56:03 +0530

    updated

---
+++ config/main_file.py
@@ -0,0 +1,9 @@
+import yaml
+
+with open ("config.yaml",'r') as file:
+    config = yaml.safe_load(file)
+
+
+    print("App name:", config.get('app_name'))
+    print('Version:',config.get('version'))
+    print('Author :', config.get('author'))
